bf5ba1d85e3c451b60160b38c443954178ceb5e6
Diego Santos <diego.net10@hotmail.com>
2015-05-28 12:12:34 -0500

    Flash card game pages modifications

--- admin/flashcardgames.php
+++ admin/flashcardgames.php
@@ -5,8 +5,8 @@ function flashcardgame_settings_page() {
     $games = get_decks();
 
     $semantic         = WP_PLUGIN_URL . '/vocabulary-plugin/css/semantic.css';
-    $new_word_url  = admin_url() . "admin.php?page=new_flashcardgame_settings";
-    $edit_word_url = admin_url() . "admin.php?page=edit_flashcardgame_settings";
+    $new_game_url  = admin_url() . "admin.php?page=new_flashcardgame_settings";
+    $edit_game_url = admin_url() . "admin.php?page=edit_flashcardgame_settings";
     ?>
 
     <!-- style sheet so our admin page looks nice -->
@@ -18,11 +18,10 @@ function flashcardgame_settings_page() {
     <?php endif ?>
 
     <h2>Games</h2>
-    <table id="wordTable" class="ui table segment tablesorter">
+    <table id="gameTable" class="ui table segment tablesorter">
         <thead>
         <tr>
             <th>Name</th>
-            <th>Image</th>
             <th>Number of Words</th>
             <th>Edit</th>
             <th>Delete</th>
@@ -31,18 +30,17 @@ function flashcardgame_settings_page() {
 
         <tbody>
         <?php foreach ($games as $game): ?>
-
+            <?php $current_game = build_deck($game->id); ?>
             <tr>
-                <td><?php echo $game->name ?></td>
-                <td><?php echo $game->image ?></td>
-                <td><?php echo $game->number_of_words ?></td>
+                <td><?php echo $current_game->get_name(); ?></td>
+                <td><?php echo $current_game->get_number_of_words(); ?></td>
 
-                <td><a class="ui blue icon button" href="<?php echo $edit_word_url ?>&id=<?php echo $game->id ?>" style="padding: 7px">Edit</a></td>
+                <td><a class="ui blue icon button" href="<?php echo $edit_game_url ?>&id=<?php echo $current_game->get_id() ?>" style="padding: 7px">Edit</a></td>
                 <td>
-                    <form method="post" action="admin-post.php" id="delete_word_form<?php echo $game->id ?>">
+                    <form method="post" action="admin-post.php" id="delete_word_form<?php echo $current_game->get_id() ?>">
                         <!-- word processing hook -->
                         <input type="hidden" name="action" value="delete_word" />
-                        <input type="hidden" name="word_id" value="<?php echo $game->word_id ?>" />
+                        <input type="hidden" name="word_id" value="<?php echo $current_game->get_id() ?>" />
 
                         <input type="submit" class="ui blue icon button" value="Delete" style="padding: 7px" >
                     </form>
@@ -52,11 +50,11 @@ function flashcardgame_settings_page() {
         <?php endforeach; ?>
         </tbody>
     </table>
-    <a class="ui blue icon button" href="<?php echo $new_word_url ?>" style="padding: 7px">New Word</a>
+    <a class="ui blue icon button" href="<?php echo $new_game_url ?>" style="padding: 7px">New Game</a>
 
     <script>
         jQuery(document).ready(function(){
-            jQuery("#wordTable").tablesorter();
+            jQuery("#gameTable").tablesorter();
         })
     </script>
 
