f6e3cfee0ca41123a41cfd453eaa33e151442150
Julia <juhpassamani@hotmail.com>
2015-06-04 13:15:15 -0500

    Version for the hotspot

--- hatpleh/db.php
+++ hatpleh/db.php
@@ -1,7 +1,7 @@
 <?php
 	define('DB_HOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASS','root');
+    define('DB_USER','wordpress');
+    define('DB_PASS','wordpress');
     define('DB_NAME','wordpress');
 
     function database_connection()
@@ -9,12 +9,29 @@
     	return new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     }
 
-    function select_word($db, $id)
-   {
-       $query = "SELECT word, hint, image, profession FROM wp_pano_trade_type AS trade, wp_pano_terms ";
-       $query .= "AS terms WHERE trade.id = terms.trade_id AND terms.id = {$id}";
-       $result = $db->query($query);
-       $row = $result->fetch_object(); 
-       return $row;
-   }
+    function get_hatpleh_words($db, $deck_id)
+    {
+        $word_table_name = 'wp_wordpleh_dictionary';
+        $deck_word_table_name = 'wp_worpleh_deck_words';
+        $deck_table_name = 'wp_wordpleh_deck';
+        $final = array();
+
+        $rows = $db->query(
+            "SELECT wdic.word, wdic.description, wdic.points, wdec.name, wdec.image
+              FROM {$word_table_name} wdic
+              INNER JOIN {$deck_word_table_name} wdecw
+              ON wdic.id = wdecw.dictionary_id
+              INNER JOIN {$deck_table_name} wdec
+              ON wdec.id = wdecw.deck_id
+              WHERE wdec.id = {$deck_id}");
+
+        while($row = $rows->fetch_object())
+        {
+            array_push($final, array('word' => $row->word,'description' => $row->description, 
+                'points' => $row->points, 'name' => $row->name, 'image' => $row->image));
+        }
+
+        return $final;
+    }
+
 ?>
