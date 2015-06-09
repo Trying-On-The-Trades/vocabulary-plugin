58f2e4261c155af0ec8b01c938e7ebe94f74e67b
Julia <juhpassamani@hotmail.com>
2015-06-09 10:37:03 -0500

    Select a domain and optional category for a new game

--- admin/new_hatplehgame.php
+++ admin/new_hatplehgame.php
@@ -3,6 +3,7 @@
 function new_hatplehgame_settings_page() {
     $semantic = WP_PLUGIN_URL . '/vocabulary-plugin/css/semantic.css';
     $categories = get_word_categories();
+    $domains = get_domains();
     $words = get_words();
 
     ?>
@@ -33,12 +34,18 @@ function new_hatplehgame_settings_page() {
 
         <div class="ui form">
 	      <div class="field">
-	        <label for="category_id">Filter by</label>
+	        <label for="filter">Filter by</label>
 	        <select name="category_id" id="category_id">
 				 <option value="NA">Select a Category</option>
                  <?php foreach($categories as $category): ?>
                     <option value="<?php echo $category->id ?>"><?php echo $category->name ?></option>
                 <?php endforeach; ?>
+            </select>
+            <select name="domain_id" id="domain_id">
+				 <option value="NA">Select a Domain</option>
+                 <?php foreach($domains as $domain): ?>
+                    <option value="<?php echo $domain->id ?>"><?php echo $domain->name ?></option>
+                 <?php endforeach; ?>
             </select>
           </div>
         </div>
@@ -48,8 +55,8 @@ function new_hatplehgame_settings_page() {
 	        <ul>
             <?php foreach($words as $word): ?>
                 <li class="games_form">
-                    <input type="checkbox" id="<?php echo $word->id ?>" class="cat<?php echo $word->word_category_id ?>" name="words[]" value="<?php echo $word->id ?>">
-                    <label for="<?php echo $word->id ?>" class="cat_option cat<?php echo $word->word_category_id ?>" ><?php echo $word->word ?></label>
+                    <input type="checkbox" id="<?php echo $word->id ?>" class="dom<?php echo $word->domain_id ?> cat<?php echo $word->word_category_id ?>" name="words[]" value="<?php echo $word->id ?>">
+                    <label for="<?php echo $word->id ?>" class="dom_option cat_option dom<?php echo $word->domain_id ?> cat<?php echo $word->word_category_id ?>" ><?php echo $word->word ?></label>
                 </li>
             <?php endforeach; ?>
             </ul>
@@ -68,34 +75,43 @@ function new_hatplehgame_settings_page() {
 			jQuery("#quest_id").val(quest_id);
 		});
 	});
-    function addForm() {
-        document.getElementById("buttonDomain").style.display = "none";
-        document.getElementById("domain_form").style.display = "block";
-    }
 
     jQuery("#category_id").change(function(){
         filter_words();
     });
 
-    function filter_words(){
+    jQuery("#domain_id").change(function(){
+        filter_words();
+    });
 
-        var selected = jQuery( "#category_id option:selected" ).val();
+function filter_words()
+{
+    var cat_selected = jQuery( "#category_id option:selected" ).val();
+    var dom_selected = jQuery( "#domain_id option:selected" ).val();
 
-        //jQuery("input:checkbox").hide();
-        //jQuery("label").hide();
+    jQuery(".cat_option").hide();
+    //jQuery("input:checkbox").hide();
 
-        if(selected == "NA"){
-            jQuery(".cat_option").show();
-            jQuery("input:checkbox").hide();
-        }else{
-            jQuery(".cat_option").hide();
-            var category = ".cat" + selected;
-
-            jQuery(category).show();
-            jQuery("input:checkbox").hide();
-        }
-
-
+    if(cat_selected == "NA" && dom_selected == "NA")
+    {
+        jQuery(".cat_option").show();
     }
+    else if(cat_selected != "NA" && dom_selected == "NA")
+    {
+        jQuery(".cat_option").hide();
+        jQuery(".cat" + cat_selected).show();
+    }
+    else if(cat_selected == "NA" && dom_selected != "NA")
+    {
+        jQuery(".cat_option").hide();
+        jQuery(".dom" + dom_selected).show();
+    }
+    else
+    {
+        jQuery(".cat_option").hide();
+        jQuery(".dom" + dom_selected + ".cat" + cat_selected).show();
+    }
+    jQuery("input:checkbox").hide();
+}
 </script>
 <?php }
